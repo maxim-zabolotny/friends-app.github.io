1c16fb494aacb21742af6f38925021d4c15659df
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-12-24 09:21:36 +0200

    add functions getUsers and renderUsers

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
-document.addEventListener('DOMContentLoaded', main)
+const users = document.querySelector('.users')
 
-function main() {
-    
+
+const url = 'https://randomuser.me/api/?results=30'
+let allUsers = []
+const currentUsers = []
+
+const getUsers = async () => {
+    try {
+        const res = await fetch(url);
+        const { results } = await res.json();
+        allUsers = [...results];
+    } catch (error) {
+        // mainContent.innerHTML = `<p>Oops! Here is the error ${error}</p>`;
+        console.log(error)
+    }
 }
+const renderUsers = (arr) => {
+    let user = ''
+
+    arr.forEach(({id ,name, picture, dob, email, cell}) => {
+        let currentUser = `<div class="user" id=${id}>
+            <img src=${picture.large} alt="user_logo" class="user_logo" />
+            <p class="user-name">${name.first} ${name.last}</p>
+            <p class="user-age">${dob.age} y.o</p>
+            <p class="user-email">${email}</p>
+            <p class="user-phone">${cell}</p>
+        </div>`
+        user += currentUser
+    });
+    users.innerHTML = user
+}
+
+const initApp = async () => {
+    await getUsers();
+    console.log(allUsers)
+    renderUsers(allUsers);
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    initApp();
+    // sortName.addEventListener('click', showFilteredUsers);
+    // sortAge.addEventListener('click', showFilteredUsers);
+    // searchInput.addEventListener('input', showFilteredUsers);
+    // filterGender.addEventListener('click', showFilteredUsers);
+  })
